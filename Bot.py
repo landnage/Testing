7b7c792cd4b9b72d44bc6a05c2ac10777cb673ca
landnage <samnangkhath5@gmail.com>
2019-03-30 23:26:25 +0700

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,24 +1,39 @@
 import discord
-from discord.ext.commands import Bot
 from discord.ext import commands
 import asyncio
-import time
 
-bot = commands.Bot(command_prefix = ".")
+bot = commands.Bot(command_prefix=".", status=discord.Status.idle, activity=discord.Game(name="Booting..."))
+bot.remove_command("help")
 
 @bot.event
 async def on_ready():
+    await bot.change_presence(status=discord.Status.online, activity=discord.Game(name="Active"))
     print("Your Bot is ready to use ")
-    print("Welcome Admin! ")
+
 
 @bot.event
 async def on_message(message):
 	author = message.author
 	content = message.content
 	print('{}: {}'.format(author, content))
-	if message.content.startswith('.pings'):
+	if message.content.startswith('.ping'):
 		await message.channel.send("Pong!")
 	elif message.content.startswith('.Hello'): 
 		await message.channel.send('Hi, How are you today ? ')
+
+@bot.command()
+async def user(ctx, member:discord.User = None):
+    if member == None:
+        member = ctx.message.author
+        pronoun = "Your"
+    else:
+        pronoun = "Their"
+    name = """{member.name}#{member.discriminator}"""
+    status = member.status
+    joined = member.joined_at
+    role = member.top_role
+    await ctx.channel.send("{pronoun} name is {name}. {pronoun} status is {status}. They joined at {joined}. {pronoun} rank is {role}.")
+
+
 
 bot.run("NTYxMDUxMDQ1NzUzNjUxMjMw.D3-m0w.4JGSFXNe5jxutaxo6DmmkaKa3ig") 
